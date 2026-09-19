7765e9c79dc151a8be193ade8b326eeea4ff57f5
Etwin Feoh <xcode6six@gmail.com>
2026-09-19 21:45:26 +0800

    feat: connect admin report routes

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Admin\InstansiController;
 use App\Http\Controllers\Admin\MagangController;
 use App\Http\Controllers\Admin\DataMagangController;
 use App\Http\Controllers\Admin\MonitoringController;
+use App\Http\Controllers\Admin\LaporanController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Dosen\DashboardController as DosenDashboardController;
 use App\Http\Controllers\Dosen\LogbookController as DosenLogbookController;
@@ -30,7 +31,8 @@ Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.')->grou
     Route::get('/magang/{magang}', [DataMagangController::class, 'show'])->name('magang.show');
     Route::patch('/magang/{magang}/status', [DataMagangController::class, 'updateStatus'])->name('magang.status');
     Route::get('/monitoring', [MonitoringController::class, 'index'])->name('monitoring.index');
-    Route::view('/laporan', 'shared.module', ['pageTitle' => 'Laporan', 'pageDescription' => 'Lihat dan kelola laporan sistem magang.'])->name('laporan.index');
+    Route::get('/laporan', [LaporanController::class, 'index'])->name('laporan.index');
+    Route::get('/laporan/{laporan}/download', [LaporanController::class, 'download'])->name('laporan.download');
     Route::view('/pengaturan', 'shared.module', ['pageTitle' => 'Pengaturan', 'pageDescription' => 'Kelola pengaturan sistem informasi magang.'])->name('pengaturan.index');
 });
 
