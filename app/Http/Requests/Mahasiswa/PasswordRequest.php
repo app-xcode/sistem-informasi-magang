ba9a5df91dbb16d48c5d0958aefc7052353e3370
Etwin Feoh <xcode6six@gmail.com>
2026-09-19 22:09:43 +0800

    feat: add mahasiswa password validation

---
+++ app/Http/Requests/Mahasiswa/PasswordRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Requests\Mahasiswa;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class PasswordRequest extends FormRequest
+{
+    public function authorize(): bool { return auth()->check(); }
+
+    public function rules(): array
+    {
+        return [
+            'current_password' => ['required','current_password'],
+            'password' => ['required','confirmed',Password::min(8)],
+        ];
+    }
+}
