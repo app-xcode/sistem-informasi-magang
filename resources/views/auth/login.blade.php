faf60bb59ab829400b8fc19552c84ba22670eb6e
x.code <xcode6six@gmail.com>
2026-09-20 13:55:37 +0800

    up icon

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,10 +62,8 @@
                 {{-- Mobile Branding --}}
                 <div class="mb-8 lg:hidden">
                     <div class="mb-5 flex h-12 w-12 items-center justify-center rounded-xl bg-slate-600 text-white shadow-lg shadow-slate-600/20">
-                        <svg class="h-6 w-6" fill="none" stroke="currentColor" stroke-width="1.8"
-                            viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M12 6.5v11M6.5 10h11M5 4.5h14A1.5 1.5 0 0 1 20.5 6v12A1.5 1.5 0 0 1 19 19.5H5A1.5 1.5 0 0 1 3.5 18V6A1.5 1.5 0 0 1 5 4.5Z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" stroke="none">
+                            <path d="M19 2.01H6c-1.206 0-3 .799-3 3v14c0 2.201 1.794 3 3 3h15v-2H6.012C5.55 19.998 5 19.815 5 19.01c0-.101.009-.191.024-.273.112-.575.583-.717.987-.727H20c.018 0 .031-.009.049-.01H21V4.01c0-1.103-.897-2-2-2zm0 14H5v-11c0-.806.55-.988 1-1h7v7l2-1 2 1v-7h2v12z"></path>
                         </svg>
                     </div>
 
